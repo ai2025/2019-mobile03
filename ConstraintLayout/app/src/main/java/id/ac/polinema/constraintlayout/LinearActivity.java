f78716467c7c47363003271e9ab2a752f7838d77
ai2025 <aisyatushshofiah@gmail.com>
2020-02-11 17:43:15 +0700

    linear commit

---
+++ ConstraintLayout/app/src/main/java/id/ac/polinema/constraintlayout/LinearActivity.java
@@ -0,0 +1,14 @@
+package id.ac.polinema.constraintlayout;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LinearActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_linear);
+    }
+}
